76ea37f0a5abc567156c4031453d9a5a820851bf
Roan Mertens <roanmertens91@gmail.com>
2019-07-07 11:43:41 +0100

    added a Parser class and the parser test frame

---
+++ spec/parser_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/parser'
+
+describe Parser do
+  context 'when parsing a file' do
+    it 'should accept a .log file and return an array' do
+    end
+    it 'should strip the \n from the strings in the array' do
+    end
+    it 'should raise a "File not found" error when the location of the file is wrong' do
+    end
+  end
+end
