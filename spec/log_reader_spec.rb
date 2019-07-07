e075dbe266c040beadcb1b6ec69d0125df5e7f4e
Roan Mertens <roanmertens91@gmail.com>
2019-07-07 17:46:17 +0100

    added spaces to tests for readability

--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -1,11 +1,5 @@
 require_relative '../lib/log_reader'
 
-# LogReader
-# should be initialized with the path to the .log file
-# if not: should raise an error
-# should return an ordered list/array of webpages with entries
-# same buth with unique entries
-
 describe LogReader do
   webserver = LogReader.new('./data/webserver.log')
   one_item = LogReader.new('./data/one_item.log')
@@ -16,21 +10,26 @@ describe LogReader do
       expect(LogReader).to receive(:new).with(instance_of(String))
       LogReader.new('./data/webserver.log')
     end
+
     it 'should raise a "File not Found" exception when the location of the file is wrong or missing' do
       expect { LogReader.new('nothing.rb') }.to raise_error('File not Found')
     end
   end
+
   context 'when asking for an ordered list of webpages with entries' do
     describe '#all' do
       it 'should return an array' do
         expect(webserver.all).to be_an_instance_of(Array)
       end
+
       it 'should include the path' do
         expect(one_item.all[0]).to include('/help_page/1')
       end
+
       it 'should return an array where each item is structured this way: path, number of views, visits' do
         expect(one_item.all[0]).to eql('/help_page/1 1 visits')
       end
+
       ten_items_parsed_and_sorted = ['/help_page/1 5 visits',
                                        '/about/2 3 visits',
                                        '/home 2 visits',]
@@ -43,12 +42,15 @@ describe LogReader do
       it 'should return an array' do
         expect(webserver.unique).to be_an_instance_of(Array)
       end
+
       it 'should include the path' do
         expect(one_item.unique[0]).to include('/help_page/1')
       end
+
       it 'should return an array where each item is structured this way: path, number of views, unique views' do
         expect(one_item.unique[0]).to eql('/help_page/1 1 unique views')
       end
+
       it 'should return an array that is sorted by number of unique entries to the page (descending)' do
         ten_items_parsed_and_sorted = ['/about/2 3 unique views',
                                        '/help_page/1 2 unique views',
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,9 +5,11 @@ describe Parser do
     it 'should accept a .log file and return an array' do
       allow(Parser).to receive(:parse).with('./data/webserver.log').and_return(an_instance_of(Array))
     end
+
     it 'should strip the \n from the strings in the array' do
       expect(Parser.parse('./data/one_item.log').join).not_to include('\n')
     end
+
     it 'should raise a "File not found" error when the location of the file is wrong' do
       expect { Parser.parse('nothing.rb') }.to raise_error('File not Found')
     end
