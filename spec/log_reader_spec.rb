5b0f5254bfa094321a9f8478662ecd19596296be
Roan Mertens <roanmertens91@gmail.com>
2019-07-07 11:17:32 +0100

    fixed tests

--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -14,7 +14,7 @@ describe LogReader do
   context 'when initializing a new LogReader instance' do
     it 'should be initialized with the path to the .log file' do
       expect(LogReader).to receive(:new).with(instance_of(String))
-      LogReader.new('../data/webserver.log')
+      LogReader.new('./data/webserver.log')
     end
     it 'should raise a "File not Found" exception when the location of the file is wrong or missing' do
       expect { LogReader.new('nothing.rb') }.to raise_error('File not Found')
@@ -23,7 +23,7 @@ describe LogReader do
   context 'when asking for an ordered list of webpages with entries' do
     describe '#all' do
       it 'should return an array' do
-        expect(webserver.all[0]).to be_an_instance_of(Array)
+        expect(webserver.all).to be_an_instance_of(Array)
       end
       it 'should include the path' do
         expect(one_item.all[0]).to include('/help_page/1')
@@ -31,12 +31,11 @@ describe LogReader do
       it 'should return an array where each item is structured this way: path, number of views, visits' do
         expect(one_item.all[0]).to eql('/help_page/1 1 visits')
       end
+      ten_items_parsed_and_sorted = ['/help_page/1 5 visits',
+                                       '/about/2 3 visits',
+                                       '/home 2 visits',]
       it 'should return an array that is sorted by number of entries to the page (descending)' do
-        ten_items_parsed_and_sorted = ['/help_page/1 5 visits',
-                                       '/about/2 4 visits',
-                                       '/home 2 visits',
-                                       '/contact 1 visits']
-        expect(tem_items.all).to eql(ten_items_parsed_and_sorted)
+        expect(ten_items.all).to eql(ten_items_parsed_and_sorted)
       end
     end
 
@@ -48,14 +47,14 @@ describe LogReader do
         expect(one_item.unique[0]).to include('/help_page/1')
       end
       it 'should return an array where each item is structured this way: path, number of views, unique views' do
-        expect(one_item.all[0]).to eql('/help_page/1 1 visits')
+        expect(one_item.unique[0]).to eql('/help_page/1 1 visits')
       end
       it 'should return an array that is sorted by number of unique entries to the page (descending)' do
-        ten_items_parsed_and_sorted = ['/help_page/1 5 visits',
-                                       '/about/2 4 visits',
-                                       '/home 2 visits',
-                                       '/contact 1 visits']
-        expect(tem_items.all).to eql(ten_items_parsed_and_sorted)
+        ten_items_parsed_and_sorted = ['/about/2 3 unique views',
+                                       '/help_page/1 3 unique views',
+                                       '/home 1 unique views',
+                                       '/contact 1 unique views']
+        expect(ten_items.unique).to eql(ten_items_parsed_and_sorted)
       end
     end
   end
