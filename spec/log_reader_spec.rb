9dc25fd438933951aaaa9522270c65863060ee12
Roan Mertens <roanmertens91@gmail.com>
2019-07-07 10:06:25 +0100

    added tests for #all method

--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -17,19 +17,26 @@ describe LogReader do
       LogReader.new('../data/webserver.log')
     end
     it 'should raise an exception when the location of the file is wrong' do
-      expect(LogReader.new('wrong path')).to raise_error
+      expect(LogReader.new('wrong path')).to raise_error('File not found')
     end
   end
   context 'when asking for an ordered list of webpages with entries' do
     describe '#all' do
       it 'should return an array' do
-        expect(webserver.all).to be_an_instance_of(Array)
+        expect(webserver.all[0]).to be_an_instance_of(Array)
       end
       it 'should include the path' do
+        expect(one_item.all[0]).to include('/help_page/1')
       end
       it 'should return an array where each item is structured this way: path, number of views, visits' do
+        expect(one_item.all[0]).to eql('/help_page/1 1 visits')
       end
       it 'should return an array that is sorted by number of entries to the page (descending)' do
+        ten_items_parsed_and_sorted = ['/help_page/1 5 visits',
+                                       '/about/2 4 visits',
+                                       '/home 2 visits',
+                                       '/contact 1 visits']
+        expect(tem_items.all).to eql(ten_items_parsed_and_sorted)
       end
     end
 
