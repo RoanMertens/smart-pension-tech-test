614df39139aa43b6c485ef79ffef0049962015da
Roan Mertens <roanmertens91@gmail.com>
2019-07-07 10:45:15 +0100

    added parse and check file method

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -1,4 +1,16 @@
 class LogReader
   def initialize(file_path)
+    check_file(file_path)
+    @file = parse(file_path)
+  end
+
+  private
+
+  def parse(file_path)
+    File.open(file_path)
+  end
+
+  def check_file(file_path)
+    raise 'File not Found' unless File.exist?(file_path)
   end
 end
